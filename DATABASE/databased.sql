e7a5067979f85acfc654b95d91caa93935d5233a
jason <caviezelj@bzz.ch>
2022-04-30 10:53:24 +0200

    Renamed database to databased.sql

--- DATABASE/databased.sql
+++ DATABASE/databased.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2019 at 03:47 PM
+-- Generation Time: April 5, 2019 at 03:47 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `db_instalike`
+-- Database: `databased`
 --
 
 -- --------------------------------------------------------
@@ -37,16 +37,6 @@ CREATE TABLE IF NOT EXISTS `tbl_photos` (
 --
 -- Dumping data for table `tbl_photos`
 --
-
-INSERT INTO `tbl_photos` (`img_id`, `img_name`, `img_path`, `img_type`, `img_title`) VALUES
-(19, 'a2.jpg', 'img/a2.jpg', 'image/jpeg', 'Captain America'),
-(20, 'ironSpiderPopHeadArtwork.jpg', 'img/ironSpiderPopHeadArtwork.jpg', 'image/jpeg', 'Iron Spider-Man'),
-(21, 'venom.jpg', 'img/venom.jpg', 'image/jpeg', 'Venom'),
-(22, 'thanosnew.jpg', 'img/thanosnew.jpg', 'image/jpeg', 'Thanos'),
-(24, 'thomyorkkk.jpg', 'img/thomyorkkk.jpg', 'image/jpeg', 'Thom Yorke'),
-(26, 'thorgroot.jpg', 'img/thorgroot.jpg', 'image/jpeg', 'Thor, Groot & Rocket - Wakanda Scene'),
-(27, 'drstrange.jpg', 'img/drstrange.jpg', 'image/jpeg', 'Doctor Strange'),
-(28, 'bp-2.jpg', 'img/bp-2.jpg', 'image/jpeg', 'Black Panther');
 
 --
 -- Indexes for dumped tables
--- dbc.php
+++ dbc.php
@@ -1,7 +1,7 @@
 <?php
 //database connection
 try {
-$dbc = new PDO('mysql:host=localhost; dbname=db_instalike', 'root', '');
+$dbc = new PDO('mysql:host=localhost; dbname=databased', 'root', '');
 $dbc->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 } catch(PDOException $e) {
 	echo "Error: " . $e->getMessage();
